fa5590964e74f1e48f11e8bbf65bd69e19f07367
Patryk Bejcer <patryk.bejcer@gmail.com>
2024-01-24 17:21:46 +0100

    Fix footer position to center

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer z-10 border border-l-transparent border-r-transparent border-t-[#33353F] text-white'>
-      <div className='container flex justify-between p-12'>
+      <div className='container mx-auto flex justify-between px-4 py-12'>
         <span>[ PB ]</span>
         <p className='text-slate-600'>All rights reserved.</p>
       </div>
